7b337c54fa0486095e9e7d71df1a6507518cd6f5
Jeremy Krovitz <jkrovitz@JeremyWliamComp.hitronhub.home>
2021-10-25 01:33:39 -0500

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export function calculateTotal (items, tax) {
-  // TODO
+ export function calculateTotal(items, tax) {
+	 let total = 0;
+	 let costOfItem = 0;
+	 for (let i = 0; i < items.length; i++) {
+		if (items[i].taxable === true) {
+			costOfItem =(items[i].price).valueOf() + (((items[i].price).valueOf()) *  ((Math.abs(tax).valueOf())))	
+		}
+		else {
+			costOfItem = items[i].price.valueOf()
+		}
+		 total = total.valueOf() +  costOfItem.valueOf()
+	 }
+	 return total.valueOf()
+
 }
