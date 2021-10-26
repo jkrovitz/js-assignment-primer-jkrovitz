465efaa70fea607f0ff1b06d954cb4a971644dd0
Jeremy Krovitz <jkrovitz@JeremyWliamComp.hitronhub.home>
2021-10-26 11:28:58 -0500

    Clean up code for assignment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
  export function calculateTotal(items, tax) {
-	 let total = 0;
-	 let costOfItem = 0;
-	 for (let i = 0; i < items.length; i++) {
-		if (items[i].taxable === true) {
-			costOfItem =(items[i].price).valueOf() + (((items[i].price).valueOf()) *  ((Math.abs(tax).valueOf())))	
-		}
-		else {
-			costOfItem = items[i].price.valueOf()
-		}
-		 total = total.valueOf() +  costOfItem.valueOf()
-	 }
-	 return total.valueOf()
-
-}
+     let total = 0;
+     for (const item of items) {
+         const { price, taxable } = item;
+         if (taxable) {
+             total += price * Math.abs(tax);
+         }
+         total += price
+     }
+     return total;
+ }
